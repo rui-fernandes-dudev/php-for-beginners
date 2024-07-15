597e2566bee5d2988be91d49345c1687e4024a4d
rui-fernandes-dudev <rui.fernandes@dudev.pt>
2024-07-15 15:51:52 +0100

    Conditionals and Booleans

--- resources/views/index.php
+++ resources/views/index.php
@@ -2,20 +2,32 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+        <title>Conditionals and Booleans</title>
+        <style>
+            body{
+                display: grid;
+                place-items: center;
+                height: 100vh;
+                margin: 0;
+                font-family: sans-serif;
+            }
+        </style>
     </head>
     <body>
+        <?php
+            $name = "Dark Matter";
+            $read = true;
+
+            if ($read) {
+                $message = "You have read $name";
+            } else {
+                $message = "You have not read $name";
+            }
+
+        ?>
 
         <h1>
-            <?php
-                $greeting = "Hello";
-
-                echo "$greeting, everybody!";
-            ?>
+            <?= $message ?>
         </h1>
-
     </body>
 </html>
